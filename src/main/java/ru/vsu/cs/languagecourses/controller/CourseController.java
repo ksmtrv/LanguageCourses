d3a161003dcdddc3ab4bd71ac11c771b61c96c98
ksmtrv <smotrova-kristina@mail.ru>
2023-02-26 15:17:12 +0300

    260223

--- src/main/java/ru/vsu/cs/languagecourses/controller/CourseController.java
+++ src/main/java/ru/vsu/cs/languagecourses/controller/CourseController.java
@@ -1,9 +1,35 @@
 package ru.vsu.cs.languagecourses.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.vsu.cs.languagecourses.dto.CourseDto;
+import ru.vsu.cs.languagecourses.dto.UsersDto;
+import ru.vsu.cs.languagecourses.entity.Course;
+import ru.vsu.cs.languagecourses.entity.Users;
+import ru.vsu.cs.languagecourses.service.CourseService;
+
+import java.util.List;
 
 
 @RestController
 public class CourseController {
+    private final CourseService service;
 
+    public CourseController(CourseService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/courses")
+    public List<CourseDto> getUsers() {
+        return service.getAllCourses();
+    }
+
+    @PostMapping("/course/new")
+    public void addNewCourse(@RequestBody Course course) {
+        service.saveNewCourse(course);
+    }
+
+    @PutMapping("/course/{id}")
+    public void updateCourse(@PathVariable Long id, @RequestBody Course course) {
+        service.updateCourse(id, course);
+    }
 }
--- src/main/java/ru/vsu/cs/languagecourses/controller/IntensityController.java
+++ src/main/java/ru/vsu/cs/languagecourses/controller/IntensityController.java
@@ -1,4 +1,8 @@
 package ru.vsu.cs.languagecourses.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IntensityController {
+
 }
